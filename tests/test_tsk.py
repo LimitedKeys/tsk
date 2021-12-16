edce767da26aa6490fd0bd29405aa8af16caf955
Rick <richardhob@gmail.com>
2021-12-16 14:21:35 -0800

    Add str to hours (float)

--- tests/test_tsk.py
+++ tests/test_tsk.py
@@ -1,3 +1,6 @@
+
+import pytest
+
 import tsk
 
 def test_parse_plan_a(plan_a):
@@ -5,3 +8,15 @@ def test_parse_plan_a(plan_a):
 
     assert result.A.time == '12 h'
     assert result.B.time == '16 h'
+
+TIME = [
+    ('1 h',  1),
+    ('1 d',  8),
+    ('1 w',  40),
+    ('1 y',  (52 * 40)),
+]
+
+@pytest.mark.parametrize("value,expected", TIME)
+def test_str_to_hours(value, expected):
+    actual = tsk.str_to_hours(value)
+    assert actual == expected
--- tsk.py
+++ tsk.py
@@ -1,7 +1,27 @@
+#!/usr/bin/python
+''' tsk: Simple Time Estimate
+
+> tsk plan.md
+Time: 1d 2h
+'''
+
 import re
 
 RE_TASK = re.compile(r'^#+ (.+)')
 RE_TIME = re.compile(r'^TIME: (.+)', re.I)
+RE_HOURS = re.compile(r'(\d+) ?([ywdh]{1})', 
+                      re.I)
+
+UNITS = {
+        'h': 1,
+        'H': 1,
+        'd': 8,
+        'D': 8,
+        'w': 40,
+        'W': 40,
+        'y': 52*40,
+        'Y': 52*40,
+        }
 
 class TimeResult(dict):
     def __init__(self, *args, **kwargs):
@@ -9,6 +29,15 @@ class TimeResult(dict):
         self.__dict__ = self
 
 def parse(path):
+    ''' Parse the provided file, find task /
+    time and return information.
+
+    Args:
+        path (str): path to file
+
+    Returns:
+        Dictionary of Tasks: {time: X}
+    '''
     tasks = TimeResult()
     with open(path, 'r') as plan:
         name = None
@@ -25,3 +54,21 @@ def parse(path):
                 tasks[name].time = estimate
 
     return tasks
+
+def str_to_hours(value):
+    '''Convert the input string into hours.
+
+    Args:
+        value (str): Time string (y w d h)
+
+    Returns:
+        Hours as a float
+    '''
+    total = 0
+    for m in RE_HOURS.finditer(value):
+        value = m.group(1)
+        unit = m.group(2)
+
+        total += float(value) * UNITS[unit]
+
+    return total
