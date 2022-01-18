0305c36223715e7a27550be96ba11290d100e09f
Rick <richardhob@gmail.com>
2022-01-18 07:13:41 -0800

    Add setup file to install tsk

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-VERSION := 1.2.0
+VERSION := 1.3.0
 ARCHIVE := tsk-$(VERSION).zip
 
 .PHONY: all pdb
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='tsk',
+    version='1.0.0',
+    description='Add up hours',
+    author='Richard Hoberecht',
+    author_email='Richardhob@gmail.com',
+    url='',
+    packages=['.'],
+    scripts=["./tsk.py"],
+    license='Use at your own risk!',
+)
