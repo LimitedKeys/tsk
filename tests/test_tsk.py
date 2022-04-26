85207c1dfc37db974e924b3156a25f9ae9093967
Rick <richardhob@gmail.com>
2022-04-26 16:20:02 -0700

    Update tests to refactor parse.
    the parse dictionary should be:
    
    - (path, name)

--- tests/test_tsk.py
+++ tests/test_tsk.py
@@ -6,10 +6,11 @@ import tsk
 def test_parse_plan_a(plan_a):
     result = tsk.parse(plan_a)
 
-    assert result.A.time == 12
-    assert result.A.tag  == "one"
-    assert result.B.time == 16
-    assert result.B.tag  == "two"
+    assert result[(plan_a, 'A')].time == 12
+    assert result[(plan_a, 'A')].tag == "one"
+
+    assert result[(plan_a, 'B')].time == 16
+    assert result[(plan_a, 'B')].tag == "two"
 
 TIME = [
     ('0.1 h', 0.1),
